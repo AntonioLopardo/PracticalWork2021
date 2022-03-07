5e15995dcd134691731eab08a33b4b9091d2abb3
AntonioLopardo <antonio.lopardo@outlook.com>
2022-03-07 18:20:19 +0000

    update the asdiv priming text

--- test_HFmodels.py
+++ test_HFmodels.py
@@ -9,33 +9,33 @@ gptj_model = "EleutherAI/gpt-j-6B"
 codeparrot_model = ""
 
 asdiv_path = "data/nlu-asdiv-dataset/dataset/ASDiv.xml"
-gsm8k_path = ""
-singleEq_path = "" 
+gsm8k_path = "data/grade-school-math/grade_school_math/data/train.jsonl"
+singleEq_path = "data/TACL2015/questions.json"
+
 
 def read_string_from_file(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.read()
 
-# Choose the dataset you want to test
-dataset_path = asdiv_path
-
-# Load the priming text to add to the prompt
-priming_text = read_string_from_file("data/priming_texts/asdiv.txt")
 
 def sample_asdiv(dataset_path):
     dom = ElementTree.parse(dataset_path)
 
-    #XML parsing
-    body_list = dom.findall('ProblemSet/Problem/Body')
-    answer_list = dom.findall('ProblemSet/Problem/Answer')
-    question_list = dom.findall('ProblemSet/Problem/Question')
-    formula_list = dom.findall('ProblemSet/Problem/Formula')
-    stype_list = dom.findall('ProblemSet/Problem/Solution-Type')
+    # XML parsing
+    body_list = dom.findall("ProblemSet/Problem/Body")
+    answer_list = dom.findall("ProblemSet/Problem/Answer")
+    question_list = dom.findall("ProblemSet/Problem/Question")
+    formula_list = dom.findall("ProblemSet/Problem/Formula")
+    stype_list = dom.findall("ProblemSet/Problem/Solution-Type")
 
-    #Randomly choose a problem
+    # Randomly choose a problem
     rand_index = np.random.randint(0, len(body_list))
 
-    return f"{body_list[rand_index].text} {question_list[rand_index].text}", formula_list[rand_index].text
+    return (
+        f"{body_list[rand_index].text} {question_list[rand_index].text}",
+        formula_list[rand_index].text,
+    )
+
 
 def sample_gsm8k(dataset_path):
     with open(dataset_path) as fh:
@@ -44,40 +44,56 @@ def sample_gsm8k(dataset_path):
     # Randomly choose a problem
     rand_index = np.random.randint(0, len(data))
     problem = data[rand_index]
-    return problem['question'], re.findall(r"#### \w+", problem["answer"])[0][5:]
+    return problem["question"], re.findall(r"#### \w+", problem["answer"])[0][5:]
+
 
 def sample_singleEq(dataset_path):
-    with open(dataset_path, 'r') as f:
+    with open(dataset_path, "r") as f:
         data = json.load(f)
-    
+
     # Randomly choose a problem
     rand_index = np.random.randint(0, len(data))
     problem = data[rand_index]
-    return problem['sQuestion'], problem['lSolutions']
+    return problem["sQuestion"], problem["lSolutions"]
 
-#sample_q, sample_a = sample_asdiv(dataset_path)
-#sample_q, sample_a = sample_asdiv(dataset_path)
-sample_q, sample_a = sample_asdiv(dataset_path)
+
+"""Choose the dataset you want to test"""
+dataset_path = gsm8k_path
+# dataset_path = singleEq_path
+# dataset_path = asdiv_path
+
+"""Load the priming text to add to the prompt and sample a question"""
+priming_text = read_string_from_file("data/priming_texts/gsm8k.txt")
+# priming_text = read_string_from_file("data/priming_texts/singleEq.txt")
+# priming_text = read_string_from_file("data/priming_texts/asdiv.txt")
+
+sample_q, sample_a = sample_gsm8k(dataset_path)
+# sample_q, sample_a = sample_singleEq(dataset_path)
+# sample_q, sample_a = sample_asdiv(dataset_path)
 
 prompt = f"{priming_text}\n\n#{sample_q}"
 print(prompt)
-print("\n" + "-"*100 + "\n")
+print("\n" + "-" * 100 + "\n")
 
 tokenizer = AutoTokenizer.from_pretrained(gptj_model)
 model = AutoModelForCausalLM.from_pretrained(gptj_model).eval().cuda()
 
-tokens = tokenizer(prompt, return_tensors="pt").input_ids
-generated_tokens = model.generate(tokens.long().cuda(), 
-                                  use_cache=True, 
-                                  do_sample=True, 
-                                  top_k=50, 
-                                  temperature=0.3, 
-                                  top_p=0.9, 
-                                  repetition_penalty=1.125, 
-                                  min_length=1, 
-                                  max_length=len(tokens[0]) + 100, 
-                                  pad_token_id=tokenizer.eos_token_id)
+print("MODEL LOADED\n")
 
-last_tokens = generated_tokens[0][len(tokens[0]):]
+tokens = tokenizer(prompt, return_tensors="pt").input_ids
+generated_tokens = model.generate(
+    tokens.long().cuda(),
+    use_cache=True,
+    do_sample=True,
+    top_k=50,
+    temperature=0.3,
+    top_p=0.9,
+    repetition_penalty=1.125,
+    min_length=1,
+    max_length=len(tokens[0]) + 100,
+    pad_token_id=tokenizer.eos_token_id,
+)
+
+last_tokens = generated_tokens[0][len(tokens[0]) :]
 generated_text = tokenizer.decode(last_tokens)
 print("Generation:\n" + generated_text)
--- test_copilot.py
+++ test_copilot.py
@@ -1,30 +1,30 @@
 ##ASDIV
 
-#Sandra took six cups of coffee and Marcie took two cups of coffee. print the number of cups of coffee did Sandra and Marcie take in total?
+# Sandra took six cups of coffee and Marcie took two cups of coffee. print the number of cups of coffee did Sandra and Marcie take in total?
 cups_sandra = 6
 cups_marcie = 2
 total_cups = cups_sandra + cups_marcie
 print(total_cups)
 
-#Mrs. Franklin had 58 Valentines. Mrs. Franklin gave some to her students. Now she has 16. Write a program that prints how many Valentines did Mrs. Franklin give to her students?
+# Mrs. Franklin had 58 Valentines. Mrs. Franklin gave some to her students. Now she has 16. Write a program that prints how many Valentines did Mrs. Franklin give to her students?
 valentines_franklin = 58
 valentines_students = 16
 total_valentines = valentines_franklin - valentines_students
 print(total_valentines)
 
-#Susie's father repaired the bookshelves in the reading room. If he has 210 books to be distributed equally on the 10 shelves he repaired, write a program that prints how many books will each shelf contain?
+# Susie's father repaired the bookshelves in the reading room. If he has 210 books to be distributed equally on the 10 shelves he repaired, write a program that prints how many books will each shelf contain?
 books_susie = 210
 shelves_susie = 10
 books_per_shelf = books_susie / shelves_susie
 print(books_per_shelf)
 
-#Michelle likes to save money every now and then so that she has money to buy the things that she wants. One day, she decided to count her savings. She opened her piggy bank and sorted out the different coins and dollar bills. If she counted a total of 20 nickels (a nickel is equivalent to 5 cents), write a program that prints what is the total value of money does she have in nickels?
+# Michelle likes to save money every now and then so that she has money to buy the things that she wants. One day, she decided to count her savings. She opened her piggy bank and sorted out the different coins and dollar bills. If she counted a total of 20 nickels (a nickel is equivalent to 5 cents), write a program that prints what is the total value of money does she have in nickels?
 nickels = 20
 nickel_value = 5
 total_nickels = nickels * nickel_value
 print(total_nickels)
 
-#Ducks nned to eat 3.5 pounds of insects each week to survive. If there is flock of ten ducks, write a program that prints how many pounds of insects do they need per day
+# Ducks nned to eat 3.5 pounds of insects each week to survive. If there is flock of ten ducks, write a program that prints how many pounds of insects do they need per day
 flock_ducks = 10
 pound_insects = 3.5
 insects_per_day = (flock_ducks * pound_insects) / 7
@@ -32,16 +32,16 @@ print(insects_per_day)
 
 
 ##GSM8k - Two different code suggestions for the same questions that get different parts of the answer wrong.
-##For example here the first code suggestion is missing the fraction of the drinks that is supposed to be water and the second one is missing the spill. 
+##For example here the first code suggestion is missing the fraction of the drinks that is supposed to be water and the second one is missing the spill.
 
-#I have 10 liters of orange drink that are two-thirds water and I wish to add it to 15 liters of pineapple drink that is three-fifths water. But as I pour it, I spill one liter of the orange drink. Write a program that prints how much water is  in the remaining 24 liters
+# I have 10 liters of orange drink that are two-thirds water and I wish to add it to 15 liters of pineapple drink that is three-fifths water. But as I pour it, I spill one liter of the orange drink. Write a program that prints how much water is  in the remaining 24 liters
 
 liters_orange = 10
 liters_pineapple = 15
 liters_orange_spill = 1
 liters_pineapple_spill = 0
-liters_pineapple_remaining = (liters_pineapple - liters_pineapple_spill)
-liters_orange_remaining = (liters_orange - liters_orange_spill)
+liters_pineapple_remaining = liters_pineapple - liters_pineapple_spill
+liters_orange_remaining = liters_orange - liters_orange_spill
 liters_orange_total = liters_orange_remaining + liters_pineapple_remaining
 print(liters_orange_total)
 
