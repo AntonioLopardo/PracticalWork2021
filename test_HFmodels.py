9d4629121d99c6bbbf2faa3b1d3483f5f4294e2a
AntonioLopardo <antonio.lopardo@outlook.com>
2022-03-25 13:27:02 +0000

    add code for benchmarks on asdiv and gsm8k

--- test_HFmodels.py
+++ test_HFmodels.py
@@ -12,22 +12,31 @@ from termcolor import colored
 import dataset_handler as dh
 import helper_func as hf
 
+import wandb
+
 torch.manual_seed(0)
 np.random.seed(8)
 
 gptj_model = "EleutherAI/gpt-j-6B"
 codeparrot_model = "lvwerra/codeparrot"
 
+# dataset_name = "gsm8k"
+dataset_name = "asdiv"
+# dataset_name = "SingleEq"
+
 """Load the priming text to add to the prompt and sample a question"""
-# priming_text_path = "data/priming_texts/gsm8k_onlyfullanswer.txt"
+# priming_text_path = "data/priming_texts/gsm8k/gsm8k_fewer_alt.txt"
 # priming_text = read_string_from_file("data/priming_texts/singleEq.txt")
 priming_text_path = "data/priming_texts/asdiv/asdiv.txt"
 
 print(colored("Prompt from: " + priming_text_path + "\n", "yellow"))
 
-current_dataset = dh.init_dataset_from_name("asdiv", primingtext_path=priming_text_path)
+current_dataset = dh.init_dataset_from_name(
+    dataset_name, primingtext_path=priming_text_path
+)
 
-sample_q_list, sample_a_list = current_dataset.sample_n_for_prompting(500)
+nr_samples = 100
+sample_q_list, sample_a_list = current_dataset.sample_n_for_prompting(nr_samples)
 
 print(colored(sample_q_list[0], "blue"))
 print(colored(sample_a_list[0], "white"))
@@ -46,4 +55,27 @@ k = 3
 """n = 3
 k = 3"""
 
-hf.testing_loop(n, k, current_dataset, tokenizer, model, sample_q_list, sample_a_list)
+config = {
+    "dataset": dataset_name,
+    "nr_samples": nr_samples,
+    "machine": "datacrunch",
+    "model": "GPT-J(6B) Half",
+    "n": n,
+    "k": k,
+}
+
+wandb.init(
+    project="PracticalWork",
+    entity="antoniolopardo",
+    config=config,
+    name="baseline_asdiv",
+)
+
+pass_at_k = hf.testing_loop(
+    n, k, current_dataset, tokenizer, model, sample_q_list, sample_a_list
+)
+
+wandb.log({"pass_at_k": pass_at_k})
+artifact = wandb.Artifact(name="priming_text", type="txt")
+artifact.add_file(priming_text_path)
+wandb.log_artifact(artifact)
