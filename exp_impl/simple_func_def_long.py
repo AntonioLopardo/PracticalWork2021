b13ba11fe64bd0760830c8ee76b818ca1b1798ff
AntonioLopardo <antonio.lopardo@outlook.com>
2022-04-17 21:26:11 +0000

    add long prompt implementations

---
+++ exp_impl/simple_func_def_long.py
@@ -0,0 +1,23 @@
+import numpy as np
+import re
+
+
+def sample_n_for_prompting(self, nr_entries=1):
+    rand_indexes = np.random.randint(0, len(self.data), nr_entries)
+
+    sample_a_list = []
+    sample_q_list = []
+    for rand_index in rand_indexes:
+        # sample_q_list.append("def exercise6():\n"+ '    """Write a program that returns the answer to the following question. ' + self.data[rand_index]["question"]+ '"""')
+        sample_q_list.append(
+            "def exercise6():\n" + '    """' + self.data[rand_index]["question"] + '"""'
+        )
+        sample_a_list.append(
+            re.findall(r"#### \w+", self.data[rand_index]["answer"])[0][5:]
+        )
+
+    return sample_q_list, sample_a_list
+
+
+def generate_prompt(self, entry_q):
+    return f"{self.priming_text}\n\n\n{entry_q}"
